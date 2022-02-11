539fbecb8964d22792fde5c84e18b3fb35d17ba8
tailieuweb.com <tailieuweb.com@gmail.com>
2022-02-11 08:41:32 +0700

    test

--- config/app.php
+++ config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * My project
+         */
+        Foostart\Acl\Authentication\AuthenticationServiceProvider::class,
+        Foostart\Category\CategoryServiceProvider::class,
+
     ],
 
     /*
