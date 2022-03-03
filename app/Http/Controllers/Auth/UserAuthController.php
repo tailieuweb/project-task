eb728439305c79c95e12d4a9ab2a230c7e8eace3
tailieuweb.com <tailieuweb.com@gmail.com>
2022-03-03 13:36:09 +0700

    test

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -18,9 +18,21 @@ class UserAuthController extends Controller
             return response(['error_message' => 'Incorrect info, please try again']);
         }
 
-        $token = auth()->user()->createToken('API Token')->accessToken;
+        $user = auth()->user();
+        $this->updateUserDeviceToken($user->id, $request->get('device_token') );
+        $token = $user->createToken('API Token')->accessToken;
 
         return response(['user' => auth()->user(), 'token' => $token]);
 
+    }
+
+    public function updateUserDeviceToken($user_id, $device_token = null) {
+
+        $profile_repository = \App::make('profile_repository');
+
+        $user_profile = $profile_repository->getFromUserId($user_id);
+        
+        $user_profile->device_token = $device_token;
+        $user_profile->save();
     }
 }
